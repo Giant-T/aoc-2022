d2c7d7cbae1afe9907e71092ad6187ac5f0d391c
William Boudreault <boudreaultwilliam@gmail.com>
2023-01-19 20:46:10 -0500

    Day 3 - part two

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,29 +1,35 @@
 fn main() {
-    let score = include_str!("../inputs.txt")
+    let score: u32 = include_str!("../inputs.txt")
         .lines()
-        .map(|line| {
-            let (first, second) = line.split_at(line.len() / 2);
-            let mut index_second: usize = 0;
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|elves| {
+            let possible_badges = elves
+                .first()
+                .unwrap()
+                .chars()
+                .into_iter()
+                .filter(|badge| elves[1].contains(*badge))
+                .collect::<Vec<_>>();
 
-            for char in first.chars() {
-                while index_second < second.len() && second.as_bytes()[index_second] as char != char
-                {
-                    index_second += 1;
-                }
+            let badge = elves
+                .last()
+                .unwrap()
+                .chars()
+                .into_iter()
+                .filter(|badge| possible_badges.contains(badge))
+                .collect::<Vec<_>>()
+                .first()
+                .unwrap()
+                .clone();
 
-                if index_second != second.len() {
-                    if char >= 'A' && char <= 'Z' {
-                        return (char as u32 - b'A' as u32 + 27) as u32;
-                    }
-                    return (char as u32 - b'a' as u32 + 1) as u32;
-                }
-
-                index_second = 0;
+            if badge >= 'a' {
+                return (badge as u8 - b'a') as u32 + 1 as u32;
             }
 
-            0
+            return (badge as u8 - b'A') as u32 + 27 as u32;
         })
-        .sum::<u32>();
+        .sum();
 
     println!(
         "The sum of the priorities of those item types is {}.",
