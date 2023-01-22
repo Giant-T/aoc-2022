7a6605e9666397aab912c1b1337c8958036e8262
William Boudreault <boudreaultwilliam@gmail.com>
2023-01-22 00:56:07 -0500

    Day 4 - part two

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -12,19 +12,24 @@ fn main() {
                 })
                 .collect::<Vec<_>>();
 
-            range_is_contained(&halves[0], &halves[1]) as u32
+            ranges_overlap(&halves[0], &halves[1]) as u32
         })
         .sum();
 
     println!(
-        "This is the number of fully contained ranges in pairs {}.",
+        "The number of overlapping pairs is {}.",
         number_of_contained_ranges
     );
 }
 
+#[allow(dead_code)]
 fn range_is_contained(first_half: &Vec<u32>, last_half: &Vec<u32>) -> bool {
     let first_half_contains_last = first_half[0] <= last_half[0] && first_half[1] >= last_half[1];
     let last_half_contains_first = last_half[0] <= first_half[0] && last_half[1] >= first_half[1];
 
     first_half_contains_last || last_half_contains_first
+}
+
+fn ranges_overlap(first_half: &Vec<u32>, last_half: &Vec<u32>) -> bool {
+    !(last_half[1] < first_half[0] || last_half[0] > first_half[1])
 }
